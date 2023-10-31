55e651b7ed2a0a5ec161f2d5d7fd02b4d9251ad8
Pam <phalligansims@yahoo.com>
2023-10-31 15:51:23 +0000

    fixing storybook theme issue

--- libs/storybook-host/.storybook/preview.ts
+++ libs/storybook-host/.storybook/preview.ts
@@ -2,7 +2,7 @@ import 'css/globals.css'
 
 export const parameters = {
     themes: {
-        default: 'twitter',
+        default: 'pink',
         list: [
             { name: 'pink', class: 'pink', color: '#DF6751', default: true },
             { name: 'blue', class: 'blue', color: '#3d5a80' }
