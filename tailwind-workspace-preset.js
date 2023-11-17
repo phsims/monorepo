2c46b9d8b4277a885098be9274305abed2d5ccc6
Pam <phalligansims@yahoo.com>
2023-11-17 10:22:28 +0000

    refactor for css theme

--- tailwind-workspace-preset.js
+++ tailwind-workspace-preset.js
@@ -1,7 +1,6 @@
 const { createThemes } = require('tw-colors');
 
 module.exports = {
-
     theme: {
         fontSize: {
             xs: ["0.75rem", { lineHeight: "1rem" }],
@@ -58,6 +57,7 @@ module.exports = {
                 bordertop: 'rgba(105, 120, 131, 0.16)',
                 darkgrey: 'rgba(44, 9, 11, 0.8)'
             },
-        })
+        }),
+        require('@tailwindcss/typography')
     ],
 };
