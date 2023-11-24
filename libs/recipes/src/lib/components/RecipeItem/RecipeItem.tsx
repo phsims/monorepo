a18c5f35756ec3151967b0b6fe15d0ef943bcebc
Pam <phalligansims@yahoo.com>
2023-11-24 16:17:49 +0000

    start refactor

--- libs/recipes/src/lib/components/RecipeItem/RecipeItem.tsx
+++ libs/recipes/src/lib/components/RecipeItem/RecipeItem.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { StarRating } from '@shared';
 import { Recipe } from '../../api/schemas';
+import Link from 'next/link';
 
 export function RecipeItem({
   name,
@@ -22,15 +23,19 @@ export function RecipeItem({
 
       <div className="p-2">
         <h4 className="text-2xl text-black font-semibold  mt-8">{name}</h4>
+        <StarRating {...ratingProps} />
         <p className="grow my-8">
           {description.length > 70
             ? `${description.substring(0, 70)}...`
             : description}
         </p>
 
-        <div>
-          <StarRating {...ratingProps} />
-        </div>
+        <Link
+          href={`/recipes/${id}`}
+          className="text-center text-lg font-medium text-primary mt-2 hover-underline"
+        >
+          View recipe
+        </Link>
       </div>
     </div>
   );
