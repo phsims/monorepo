465361d8055096b70c168e67fc95dedf72c7f379
Pam <phalligansims@yahoo.com>
2023-11-27 13:47:25 +0000

    submit form props

--- libs/shared/src/lib/components/molecules/SubmitForm/SubmitForm.tsx
+++ libs/shared/src/lib/components/molecules/SubmitForm/SubmitForm.tsx
@@ -2,16 +2,24 @@ import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import Image from 'next/image';
 import { InputProps } from '../../atoms/Input/Input';
 
+/* The `SubmitFormProps` interface defines the props that can be passed to the `SubmitForm` component.
+It has two properties: */
 interface SubmitFormProps {
   onSubmit: (data: any) => void;
   inputProps: InputProps;
 }
 
+/* The `export interface FormData` is defining an interface named `FormData`. This interface specifies
+the shape of the data that will be submitted in the form. In this case, it has a single property
+`text` of type `string`. This means that when the form is submitted, it will contain a field named
+`text` with a string value. */
 export interface FormData {
   text: string;
 }
 
 export function SubmitForm({ onSubmit, inputProps }: SubmitFormProps) {
+  /* The code `const { handleSubmit, control, formState: { errors } } = useForm<FormData>();` is using
+  the `useForm` hook from the `react-hook-form` library to initialize form functionality. */
   const {
     handleSubmit,
     control,
@@ -20,6 +28,12 @@ export function SubmitForm({ onSubmit, inputProps }: SubmitFormProps) {
 
   const { label, placeholder } = inputProps;
 
+  /**
+   * The function `handleFormSubmit` is a TypeScript React function that handles form submission by
+   * calling the `onSubmit` function with the form data, and catches and logs any errors that occur.
+   * @param data - The `data` parameter is of type `FormData`. It represents the data submitted in the
+   * form.
+   */
   const handleFormSubmit: SubmitHandler<FormData> = async (data) => {
     try {
       onSubmit(data);
