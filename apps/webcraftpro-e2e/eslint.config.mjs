72ed115b537381156029fbcea1f58897b38d601f
phsims <pamhsims@yahoo.com>
2026-03-22 10:04:40 +0000

    updating deps

--- apps/webcraftpro-e2e/eslint.config.mjs
+++ apps/webcraftpro-e2e/eslint.config.mjs
@@ -3,7 +3,13 @@ import baseConfig from '../../eslint.config.mjs';
 
 export default [
   playwright.configs['flat/recommended'],
+  playwright.configs['flat/recommended'],
   ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    // Override or add rules here
+    rules: {},
+  },
   {
     files: ['**/*.ts', '**/*.js'],
     // Override or add rules here
